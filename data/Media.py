42101fe15a646d9a9d94197361b579fdcb1ddc4c
Nikita <prostocurbanov@gmail.com>
2025-06-24 02:10:10 +0300

    Update Media.py
    1. изменил структуру сохранения данных в getFile для более корректного использования базы данных.

--- data/Media.py
+++ data/Media.py
@@ -4,25 +4,24 @@ import requests
 from telebot.types import InputFile
 
 def isDate(date_str):
-    if not re.fullmatch(r"\d{2}-\d{2}-\d{4}", date_str):
+    if not re.fullmatch(r"\d{4}-\d{2}-\d{2}", date_str):
         return False
     
     day, month, year = map(int, date_str.split('-'))
-    if (day, month, year) > (31, 12, 9999):
+    if (day, month, year) > (9999, 12, 31):
         return False
-    if (day, month, year) < (1, 1, 1900):
+    if (day, month, year) < (1900, 1, 1):
         return False
     return (day, month, year)
 
 class Media:
     filename: str
     filepath: str
-    NASA_API_KEY = None
     
     def __init__(self, filename:str, filepath:str, bot, NASA_API_KEY:str):
         self.filename = os.path.join(os.path.dirname(__file__), filename)
         self.filepath = filepath
-        self.NASA_API_KEY = NASA_API_KEY
+        self.NASA_API_KEY = 'DEMO_KEY'
     
     def getFile(self, date):
         type = 'NONE'
@@ -30,17 +29,13 @@ class Media:
         with open(self.filename, 'r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                if int(row['DATE']) == date:
-                    if row['ID'] == '':
-                        type = 'FnT'            # file no tg id
-                        info = row['FILEAME']
-                    else:
-                        type = 'FT'             # file and tg id
-                        info = row['ID']
-                    break
+                print(date, row['DATE'])
+                if row['DATE'] == date:
+                    return [row['LINK'], row['TEXT']]
             else:
                 type = 'NF'               # no file
                 info = date
+
         if type == 'NF':
             r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={self.NASA_API_KEY}&date={date[2]}-{date[1]}-{date[0]}&thumbs=False")
             if r.status_code != 200:
@@ -58,26 +53,26 @@ class Media:
                 if r.status_code != 200:
                     return None
                 
-                with open(os.path.join(self.filepat, ), 'wb') as file:
+                with open(os.path.join(self.filepath, ), 'wb') as file:
                     file.write(r.content)
                 
                 with open(self.filename, 'a', encoding='utf-8') as file:
-                    writer = csv.DictWriter(file, fieldnames=['ID', 'NAME', 'USERNAME', 'SUBSCRIPTION'])
+                    writer = csv.DictWriter(file, fieldnames=['ID', 'NAME', 'LINK', 'TEXT'])
                     writer.writerow({
                         'ID': date,
                         'NAME': date,
-                        'USERNAME': date,
-                        'SUBSCRIPTION': False
+                        'LINK': nasa['hdurl'],
+                        'TEXT': nasa['explanation'],
                     })
                 
-                return (self.filepath, date)
+                return [nasa['url'], nasa['explanation']]
 
                     
         
         
 def photoSender(bot, message):
     date = isDate(message.text)
-    r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={}&date={date[2]}-{date[1]}-{date[0]}&thumbs=False")
+    r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={TPCZTjkVRKllaLRbqTH8Qtq2mpHTiGAg0MVXj6Fb}&date={date[2]}-{date[1]}-{date[0]}&thumbs=False")
     if r.status_code != 200:
         print(r.status_code, r.headers)
         bot.reply_to(message, "Что-то пошло не так, возможно в другой раз")
