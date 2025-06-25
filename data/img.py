11e651988bd63d32f14e7fed0ad1ddb7d522a721
Nikita <prostocurbanov@gmail.com>
2025-06-25 03:08:11 +0300

    Update img.py
    Исправления для соответствия PEP8

--- data/img.py
+++ data/img.py
@@ -1,26 +1,36 @@
-import requests
 import os
+import requests
 
+import yt_dlp
+
+# Make a request to check if the video file is an image
 req = requests.get("https://www.youtube.com/embed/rQcKIN9vj3U?rel=0")
-
 print(req.status_code)
 print(req.headers['content-type'])
+
+# Check if the response is an image
 if 'image' in req.headers['content-type']:
     with open("test.jpg", "wb") as file:
         file.write(req.content)
         
-import yt_dlp
-
 def download(link, name='%(title)s'):
+    """
+    Download a video from the provided link.
+
+    Arguments:
+    link -- URL of the video
+    name -- desired name for the downloaded file
+    """
     ydl_opts = {
-        'format': 'bestvideo+bestaudio/best', #берем самое лучшее качество видео и фото
-        'outtmpl': '{}.%(ext)s'.format(name), #наше выбраное имя, если его не было, то стандартное - название видео на самом сайте
+        'format': 'bestvideo+bestaudio/best', # Get the best quality video and audio
+        'outtmpl': '{}.%(ext)s'.format(name), # The name chosen for the file, if not provided, will default to the video's title on the website
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(link, download=True)
         downloaded_file_path = ydl.prepare_filename(info_dict)
+        
     print(f"Видео {downloaded_file_path} успешно загружено!")
     return downloaded_file_path
 
-
+# Example download call
 print(download('https://www.youtube.com/watch?v=uHgt8giw1LY', 'Привет!'))
